e4855accedba8ddb1547584de27c33e3be207e57
Hillel Arnold <hillel.arnold@gmail.com>
2023-07-04 18:11:00 -0400

    move client instantiation inside method

--- src/handle_digitized_av_trigger.py
+++ src/handle_digitized_av_trigger.py
@@ -15,9 +15,6 @@ FORMAT_MAP = {
     'rac-av-upload-video': 'video'
 }
 
-ssm_client = boto3.client(
-    'ssm', region_name=environ.get(
-        'AWS_DEFAULT_REGION', 'us-east-1'))
 full_config_path = f"/{environ.get('ENV')}/{environ.get('APP_CONFIG_PATH')}"
 
 
@@ -32,6 +29,10 @@ def get_config(ssm_parameter_path):
     """
     configuration = {}
     try:
+        ssm_client = boto3.client(
+            'ssm', 
+            region_name=environ.get('AWS_DEFAULT_REGION', 'us-east-1'))
+        
         param_details = ssm_client.get_parameters_by_path(
             Path=ssm_parameter_path,
             Recursive=False,
