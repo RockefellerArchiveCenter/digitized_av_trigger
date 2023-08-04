fe38c32c931d7bf99409a0962f536c25be98769f
Hillel Arnold <hillel.arnold@gmail.com>
2023-08-04 13:03:37 -0400

    add handling for QC events

--- src/handle_digitized_av_trigger.py
+++ src/handle_digitized_av_trigger.py
@@ -53,25 +53,8 @@ def get_config(ssm_parameter_path):
         return configuration
 
 
-def lambda_handler(event, context):
-    """Triggers ECS task."""
-
-    bucket = event['Records'][0]['s3']['bucket']['name']
-    object = event['Records'][0]['s3']['object']['key']
-    format = FORMAT_MAP[bucket]
-
-    config = get_config(full_config_path)
-    ecs_client = boto3.client(
-        'ecs',
-        region_name=environ.get('AWS_DEFAULT_REGION', 'us-east-1'))
-
-    logger.info(
-        "Running task for event from object {} in bucket {} with format {}".format(
-            object,
-            bucket,
-            format))
-
-    response = ecs_client.run_task(
+def run_task(ecs_client, config, task_definition, environment):
+    return ecs_client.run_task(
         cluster=config.get('ECS_CLUSTER'),
         launchType='FARGATE',
         networkConfiguration={
@@ -81,30 +64,104 @@ def lambda_handler(event, context):
                 'assignPublicIp': 'DISABLED'
             }
         },
-        taskDefinition='digitized_av_validation',
+        taskDefinition=task_definition,
         count=1,
         startedBy='lambda/digitized_av_trigger',
         overrides={
             'containerOverrides': [
                 {
-                    "name": "digitized_av_validation",
-                    "environment": [
-                        {
-                            "name": "FORMAT",
-                            "value": format
-                        },
-                        {
-                            "name": "AWS_SOURCE_BUCKET",
-                            "value": bucket
-                        },
-                        {
-                            "name": "SOURCE_FILENAME",
-                            "value": object
-                        }
-                    ]
+                    "name": task_definition,
+                    "environment": environment
                 }
             ]
         }
     )
+
+
+def lambda_handler(event, context):
+    """Triggers ECS task."""
+
+    config = get_config(full_config_path)
+    ecs_client = boto3.client(
+        'ecs',
+        region_name=environ.get('AWS_DEFAULT_REGION', 'us-east-1'))
+
+    if event['Records'][0].get('s3'):
+        """Handles events from S3 buckets."""
+
+        bucket = event['Records'][0]['s3']['bucket']['name']
+        object = event['Records'][0]['s3']['object']['key']
+        format = FORMAT_MAP[bucket]
+
+        logger.info(
+            "Running validation task for event from object {} in bucket {} with format {}".format(
+                object,
+                bucket,
+                format))
+
+        environment = [
+            {
+                "name": "FORMAT",
+                "value": format
+            },
+            {
+                "name": "AWS_SOURCE_BUCKET",
+                "value": bucket
+            },
+            {
+                "name": "SOURCE_FILENAME",
+                "value": object
+            }
+        ]
+
+        response = run_task(
+            ecs_client,
+            config,
+            'digitized_av_validation',
+            environment)
+
+    elif event['Records'][0].get('Sns'):
+        """Handles events from SNS."""
+
+        attributes = event['Records'][0]['Sns']['MessageAttributes']
+
+        response = f'Nothing to do for SNS event: {event}'
+
+        if ((attributes['service']['Value'] == 'qc') and (
+                attributes['outcome']['Value'] == 'SUCCESS')):
+            """Handles QC approval events."""
+
+            format = attributes['format']['Value']
+            refid = attributes['refid']['Value']
+            rights_ids = attributes['rights_ids']['Value']
+
+            logger.info(
+                "Running packaging task for event from object {} with format {}".format(
+                    refid,
+                    format))
+
+            environment = [
+                {
+                    "name": "FORMAT",
+                    "value": format
+                },
+                {
+                    "name": "REFID",
+                    "value": refid
+                },
+                {
+                    "name": "RIGHTS_IDS",
+                    "value": rights_ids
+                }
+            ]
+
+            response = run_task(
+                ecs_client,
+                config,
+                'digitized_av_packaging',
+                environment)
+
+    else:
+        raise Exception('Unsure how to parse message')
 
     return json.dumps(response, default=str)
