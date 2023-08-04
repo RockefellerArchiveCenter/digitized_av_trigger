943267ade5d6f38683958e09169fb9ff2efd9f86
Hillel Arnold <hillel.arnold@gmail.com>
2023-08-04 13:03:49 -0400

    update tests and fixtures

--- tests/test_handle_digitized_av_trigger.py
+++ tests/test_handle_digitized_av_trigger.py
@@ -13,7 +13,7 @@ from src.handle_digitized_av_trigger import get_config, lambda_handler
 
 @mock_ecs
 @patch('src.handle_digitized_av_trigger.get_config')
-def test_args(mock_config):
+def test_s3_args(mock_config):
     test_cluster_name = "default"
     mock_config.return_value = {
         "AWS_REGION": "us-east-1",
@@ -34,7 +34,8 @@ def test_args(mock_config):
     )
 
     for fixture, expected_args in [
-            ('put_audio.json', 'audio_args.json'), ('put_video.json', 'video_args.json')]:
+            ('s3_put_audio.json', 's3_audio_args.json'),
+            ('s3_put_video.json', 's3_video_args.json')]:
         with open(Path('fixtures', fixture), 'r') as df:
             message = json.load(df)
             response = json.loads(lambda_handler(message, None))
@@ -45,6 +46,49 @@ def test_args(mock_config):
             with open(Path('fixtures', expected_args), 'r') as af:
                 args = json.load(af)
                 assert response['tasks'][0]['overrides'] == args
+
+
+@mock_ecs
+@patch('src.handle_digitized_av_trigger.get_config')
+def test_sns_args(mock_config):
+    test_cluster_name = "default"
+    mock_config.return_value = {
+        "AWS_REGION": "us-east-1",
+        "ECS_CLUSTER": test_cluster_name,
+        "ECS_SUBNET": "subnet"}
+    client = boto3.client("ecs", region_name="us-east-1")
+    client.create_cluster(clusterName=test_cluster_name)
+    client.register_task_definition(
+        family="digitized_av_packaging",
+        containerDefinitions=[
+            {
+                "name": "digitized_av_packaging",
+                "image": "docker/hello-world:latest",
+                "cpu": 1024,
+                "memory": 400,
+            }
+        ],
+    )
+
+    for fixture, expected_args in [
+            ('sns_audio_accept.json', 'sns_audio_args.json'),
+            ('sns_video_accept.json', 'sns_video_args.json')]:
+        with open(Path('fixtures', fixture), 'r') as df:
+            message = json.load(df)
+            response = json.loads(lambda_handler(message, None))
+            assert len(response['tasks']) == 1
+            assert response['tasks'][0]['startedBy'] == 'lambda/digitized_av_trigger'
+            assert response['tasks'][0][
+                'taskDefinitionArn'] == f"arn:aws:ecs:us-east-1:{DEFAULT_ACCOUNT_ID}:task-definition/digitized_av_packaging:1"
+            with open(Path('fixtures', expected_args), 'r') as af:
+                args = json.load(af)
+                assert response['tasks'][0]['overrides'] == args
+
+    for fixture in ['sns_audio_reject.json', 'sns_audio_reject.json']:
+        with open(Path('fixtures', fixture), 'r') as df:
+            message = json.load(df)
+            response = json.loads(lambda_handler(message, None))
+            assert 'Nothing to do for SNS event:' in response
 
 
 @mock_ssm
